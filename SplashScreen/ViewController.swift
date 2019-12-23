1c65f44f9a0c470ffd284d03ec3ab9db1b951b73
Brett Markowitz <batting1000@comcast.net>
2019-12-22 19:18:39 -0500

    First commit

---
+++ SplashScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplashScreen
+//
+//  Created by Brett Markowitz on 12/22/19.
+//  Copyright © 2019 Brett Markowitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
