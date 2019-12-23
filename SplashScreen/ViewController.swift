ef26efbe3458f48e4faa90345eceaedb2582c1bb
Brett Markowitz <batting1000@comcast.net>
2019-12-22 19:27:06 -0500

    Support for pre iOS 13

--- SplashScreen/ViewController.swift
+++ SplashScreen/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
