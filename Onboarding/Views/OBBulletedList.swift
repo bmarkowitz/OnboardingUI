ebec011f14ee63cc1eb1711264bc48afa68cb45c
Brett Markowitz <batting1000@comcast.net>
2020-05-08 22:22:34 -0400

    More renaming and setup

---
+++ Onboarding/Views/OBBulletedList.swift
@@ -0,0 +1,13 @@
+//
+//  OBBulletedList.swift
+//  SplashScreen
+//
+//  Created by Brett Markowitz on 5/8/20.
+//  Copyright © 2020 Brett Markowitz. All rights reserved.
+//
+
+import UIKit
+
+class OBBulletedList: UIView {
+
+}
